04a97e9fd5e9feff4dee17d0d5de2f6ceeff3d14
prashant <pra98935@gmail.com>
2017-07-23 17:31:07 +0530

    login form and model for user and localstorage

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {ApiService} from '../api.service';
+import { Usermodel } from '../model/usermodel';
 
 @Component({
   selector: 'app-login',
@@ -8,24 +9,51 @@ import {ApiService} from '../api.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-  email:string;
-  password:string;
 
-  titke = "submit";
+  responseObject;
+  
+  login: Usermodel = new Usermodel();
 
   constructor(private newService:ApiService){}
 
   ngOnInit() {
-    this.newService.fetchData()
-    .subscribe(function(t){
-      console.log(t);
-      });
+    // this.newService.fetchData()
+    // .subscribe(function(t){
+    //   console.log(t);
+    //   });
   }
 
-  addContact(){
-    console.log("submit form");
-    var submit = "form sumit";
-    return submit;
+  // addContact(){
+  //   console.log("submit form");
+  //   var submit = "form sumit";
+  //   return submit;
+  // }
+
+  insert(){
+    let self = this;
+    let data:any = "";
+    let email = this.login.email;
+    let password = this.login.password;
+    data = [{
+              "method":"login",
+              "email_id":email,
+              "password":password
+           }]  
+    this.newService.insertData(data)
+    .subscribe(function(response){
+      self.responseObject = response;
+      console.log(self.responseObject);
+      
+      if (typeof(Storage) !== "undefined" && self.responseObject.result.user_id) {
+          localStorage.setItem("account_type", self.responseObject.result.account_type);
+          localStorage.setItem("email_id", self.responseObject.result.email_id);
+          localStorage.setItem("user_id", self.responseObject.result.user_id);
+      }
+
+    });
+    
+    //console.log(this.login);
+    //console.log(this.login.email);
   }
 
 }
---
+++ src/app/model/usermodel.ts
@@ -0,0 +1,13 @@
+export class Usermodel {
+    // id: number;
+    name: string = '';
+    email: string = '';
+    password: string = '';
+
+    constructor(values: Object = {}) { //The Object.values() method returns an array of a given object's
+        
+        //The Object.assign() method only copies enumerable and own properties from a source object to a target object.
+        //Object.assign(target, ...sources)
+        Object.assign(this, values); 
+    }
+}
